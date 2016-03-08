2c807ee89b14dc23a4773d0bb789611cec67661b
baraneetharan.r <baraneetharan.ramasamy@kgfsl.com>
2016-03-08 13:27:26 +0530

    sample testcase

--- spec/first_test.js
+++ spec/first_test.js
@@ -1,19 +1,25 @@
-var assert = require('assert'),
-test = require('selenium-webdriver/testing'),
-webdriver = require('selenium-webdriver');
- 
-test.describe('Google Search', function() {
-  test.it('should work', function() {
-      this.timeout(30000);
-    var driver = new webdriver.Builder().
-    withCapabilities(webdriver.Capabilities.firefox()).
-    build();
-driver.get('http://www.google.com');
-    var searchBox = driver.findElement(webdriver.By.name('q'));
-    searchBox.sendKeys('simple nodejs programmer');
-    searchBox.getAttribute('value').then(function(value) {
-      assert.equal(value, 'simple nodejs programmer');
+ var assert = require('assert'),
+ test = require('selenium-webdriver/testing'),
+ webdriver = require('selenium-webdriver');
+//  
+// test.describe('Google Search', function() {
+//   test.it('should work', function() {
+//       this.timeout(30000);
+//     var driver = new webdriver.Builder().
+//     withCapabilities(webdriver.Capabilities.firefox()).
+//     build();
+// driver.get('http://www.google.com');
+//     var searchBox = driver.findElement(webdriver.By.name('q'));
+//     searchBox.sendKeys('simple nodejs programmer');
+//     searchBox.getAttribute('value').then(function(value) {
+//       assert.equal(value, 'simple nodejs programmer');
+//     });
+//     driver.quit();
+//   });
+// });
+
+test.describe('sample', function() {
+    test.it('should work', function() {
+        assert.equal('value', 'value');
     });
-    driver.quit();
-  });
 });
