170a764cf632986a44be95d22becd3f9b25f87e6
baraneetharan.r <baraneetharan.ramasamy@kgfsl.com>
2016-03-08 17:15:51 +0530

    separate test, open gulp tasks in travis

--- spec/first_test.js
+++ spec/first_test.js
@@ -1,22 +1,22 @@
  var assert = require('assert'),
  test = require('selenium-webdriver/testing'),
  webdriver = require('selenium-webdriver');
-//  
-// test.describe('Google Search', function() {
-//   test.it('should work', function() {
-//       this.timeout(30000);
-//     var driver = new webdriver.Builder().
-//     withCapabilities(webdriver.Capabilities.firefox()).
-//     build();
-// driver.get('http://www.google.com');
-//     var searchBox = driver.findElement(webdriver.By.name('q'));
-//     searchBox.sendKeys('simple nodejs programmer');
-//     searchBox.getAttribute('value').then(function(value) {
-//       assert.equal(value, 'simple nodejs programmer');
-//     });
-//     driver.quit();
-//   });
-// });
+ 
+test.describe('Google Search', function() {
+  test.it('should work', function() {
+      this.timeout(30000);
+    var driver = new webdriver.Builder().
+    withCapabilities(webdriver.Capabilities.firefox()).
+    build();
+driver.get('http://www.google.com');
+    var searchBox = driver.findElement(webdriver.By.name('q'));
+    searchBox.sendKeys('simple nodejs programmer');
+    searchBox.getAttribute('value').then(function(value) {
+      assert.equal(value, 'simple nodejs programmer');
+    });
+    driver.quit();
+  });
+});
 
 test.describe('sample', function() {
     test.it('should work', function() {
